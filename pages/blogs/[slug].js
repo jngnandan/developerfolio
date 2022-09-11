50a4162e9553a63fbecf4ad85c5e5579d160693d
jngnandan <jngnandan@gmail.com>
2022-09-11 03:34:23 +0100

    slug removed

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,27 +1,27 @@
-import Link from "next/link"
-import Layout from '../components/Layout'
-import {useContext} from 'react'
-import {ContentContext} from '../../context/ContentContext'
-import { useRouter } from 'next/router'
+// import Link from "next/link"
+// import Layout from '../components/Layout'
+// import {useContext} from 'react'
+// import {ContentContext} from '../../context/ContentContext'
+// import { useRouter } from 'next/router'
 
 
-const PostItem = () => {
-    const {posts} = useContext(ContentContext)
-    const { query } = useRouter();
-    const { slug } = query;
+// const PostItem = () => {
+//     const {posts} = useContext(ContentContext)
+//     const { query } = useRouter();
+//     const { slug } = query;
 
-    const post = posts.find((post) => post.id === query.slug);
-    // console.log(post)
-    const {title, description, date} = post.body
-    return(
-        <Layout>
-            <div className="my-4">
-                <h1 className="font-bold my-3 text-3xl pt-6">{title}</h1>
-                <p className="border-t my-6"></p>
-                <p className="text-md my-5">{description}</p>
-            </div>
-        </Layout>
+//     const post = posts.find((post) => post.id === query.slug);
+//     // console.log(post)
+//     const {title, description, date} = post.body
+//     return(
+//         <Layout>
+//             <div className="my-4">
+//                 <h1 className="font-bold my-3 text-4xl pt-6">{title}</h1>
+//                 <p className="border-t my-6"></p>
+//                 <p className="text-md my-5">{description}</p>
+//             </div>
+//         </Layout>
 
-    )
-}
-export default PostItem
+//     )
+// }
+// export default PostItem
