7abfaf307eefc91232845215a37476b1bc9561cd
jngnandan <jngnandan@gmail.com>
2022-09-11 04:03:31 +0100

    requesting slug post from firebase

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,18 +1,34 @@
 import Link from "next/link"
 import Layout from '../components/Layout'
-import {useContext} from 'react'
+import {useContext, useState, useEffect} from 'react'
 import {ContentContext} from '../../context/ContentContext'
 import { useRouter } from 'next/router'
+import {collection, getDocs, setDoc, doc, query, getFirestore} from 'firebase/firestore'
+import {db, auth, provider} from '../../firebase'
 
 
 const PostItem = () => {
+    const [author, setAuthor] = useState([])
+
     const {posts} = useContext(ContentContext)
     const { query } = useRouter();
     const { slug } = query;
 
-    const post = posts.find((post) => post.id === query.slug);
-    // console.log(post.body)
-    const {title, description, date} = post.body
+    // const post = posts.find((post) => post.id === query.slug);
+
+    useEffect(() => {
+        const getBooks = async () => {
+        const querySnapshot = await getDocs(collection(db, 'posts'))
+        querySnapshot.docs.map(doc => doc.id === query.slug && setAuthor(doc.data()))
+    }
+    getBooks()
+}, [])
+
+    console.log(author)
+    const {title, description, date} = author
+
+
+
     return(
         <Layout>
             <div className="my-4">
--- pages/postcard.js
+++ pages/postcard.js
@@ -10,7 +10,7 @@ function PostCard(props) {
   return (
         <div className='my-4'>
             <Link href={`/blogs/${post.id}`}>
-                <h1 className='font-semibold hover:text-red-500 my-2 text-xl'>{title}</h1>
+                <h1 className='font-semibold hover:text-red-500 my- text-xl'>{title}</h1>
             </Link>
             <p className='text-gray-500 line-clamp-3'>{description}</p>
             <p className='text-gray-500 underline hover:text-red-500'><Link href={`/blogs/${post.id}`}>More</Link></p>
